85e49fc39c7cf085986a1c6c627554498ca81f9b
鲸落 <1656855152@qq.com>
2024-08-20 20:52:05 +0800

    工程初始化

---
+++ paicoding-web/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.htx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
